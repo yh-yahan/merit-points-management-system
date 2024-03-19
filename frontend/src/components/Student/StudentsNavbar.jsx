26cf548bc7783a0b8816927519f266ab96ae43b7
yahan <yyh01126228789@gmail.com>
2024-03-19 21:37:44 +0800

    Commiting StudentsNavbar

--- frontend/src/components/Student/StudentsNavbar.jsx
+++ frontend/src/components/Student/StudentsNavbar.jsx
@@ -1,7 +1,45 @@
+import { Link, NavLink, useLocation } from 'react-router-dom'
+
 function StudentsNavbar(){
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return(
     <>
-    
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand ms-4" to="/">
+            <div style={{ maxWidth: "73px" }}>
+              <img src="src/assets/LOGO.png" className="img-fluid" alt="Logo" />
+            </div>
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className={`nav-link navLink ${isActive('/my-merit-points') ? 'active' : ''}`} to="my-merit-points">My-merit-points</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={`nav-link navLink ${isActive('/leaderboard') ? 'active' : ''}`} to="leaderboard">Leaderboard</NavLink>
+              </li>
+            </ul>
+            <ul className="navbar-nav ml-auto mb-2 mb-lg-0 me-5">
+              <li className="nav-item">
+                <NavLink className={`nav-link navLink ${isActive('/settings') ? 'active' : ''}`} to="settings">Settings</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={`nav-link navLink ${isActive('/logout') ? 'active' : ''}`} to="logout"><i className="bi bi-box-arrow-left"></i> Logout</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
